82cb444c199728d1a8d4d20d52b27277ef753d11
HackMeMan <happydude0164@gmail.com>
2023-03-19 08:09:20 -0400

    Graimaire
    a

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from flask import Flask, Response, request, send_file, make_response, send_from_directory
 import json
+<<<<<<< HEAD
+=======
+import csv
+import requests
+>>>>>>> 2dfb135 (Route optimization when set to travail)
 
 fichier = 'tickets.json'
 
@@ -68,9 +73,9 @@ def supprimer():
 
     if id != 0:
         index = None
-        for ticket in tickets:
+        for i, ticket in enumerate(tickets):
             if ticket['id'] == id:
-                index = ticket['id']
+                index = i
     else:
         index = 0
 
@@ -133,9 +138,9 @@ def modifier():
 
     if id != 0:
         index = None
-        for ticket in tickets:
+        for i, ticket in enumerate(tickets):
             if ticket['id'] == id:
-                index = ticket['id']
+                index = i
     else:
         index = 0
 
@@ -158,11 +163,73 @@ def modifier():
     with open(fichier, 'w') as f:
         f.write(json.dumps(tickets))
 
+<<<<<<< HEAD
     return Response(status=200)
+=======
+    resp = Response(status=200)
+
+    @resp.call_on_close
+    def on_close():
+        if etat == "travail":
+            with open('Bon de travail.csv', 'a', newline='') as f:
+                writer = csv.writer(f, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                writer.writerow([id, adresse.split()[0], ' '.join(adresse.split()[1:]), piece, type_bac])
+
+            visits = {}
+            for ticket in tickets:
+                if ticket['etat'] != 'travail':
+                    continue
+
+                res = requests.get(f'https://nominatim.openstreetmap.org/search?q={ticket["adresse"]}&format=json')
+                res_json = res.json()
+                visits[ticket['id']] = {
+                    "location": {
+                        "name": ticket['adresse'],
+                        "lng": res_json[0]['lon'],
+                        "lat": res_json[0]['lat'],
+                    }
+                }
+
+            fleet = {
+                "vehicle_1": {
+                    "start_location": {
+                        "id": "UQO",
+                        "name": "283 Alexandre-Taché",
+                        "lat": 45.422472,
+                        "lng": -75.738186,
+                    }
+                }
+            }
+
+            data = {
+                "visits": visits,
+                "fleet": fleet
+            }
+
+            token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDE2MzcxYWY4YWQ5YTAwMThlYjI3YjIiLCJpYXQiOjE2NzkyMjEyOTh9.FH4JTErksHAhYsKhVjcS35ZXuxY8GZQKqVP7Ni3cTeI'
+
+            headers = {
+                'Content-Type': 'application/json',
+                'Authorization': f'bearer {token}'
+            }
+
+            res = requests.post('https://api.routific.com/v1/vrp', headers=headers, data=json.dumps(data))
+
+            maps_url = 'https://www.google.com/maps/dir/'
+            for location in res.json()['solution']['vehicle_1']:
+                maps_url += location['location_name'] + '/'
+            print(maps_url.replace(' ', '%20'))
+
+    return resp
+>>>>>>> 2dfb135 (Route optimization when set to travail)
 
 @app.route('/<path:path>')
 def send_report(path):
     return send_from_directory('src/', path)
+
+@app.route('/', methods=["GET"])
+def send_index():
+    return send_from_directory('src/', 'index.html')
 
 if __name__ == "__main__":
     app.run()
